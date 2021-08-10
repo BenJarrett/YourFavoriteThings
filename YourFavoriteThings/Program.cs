19a62335b7e6c6f1673d574c6eb5b1c9f935e9a0
BenJarrett <b.cl.jarrett@gmail.com>
2021-08-10 16:25:58 -0500

    Add project files.

---
+++ YourFavoriteThings/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace YourFavoriteThings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
